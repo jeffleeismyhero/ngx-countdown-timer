3df9a5f8f66c9dd13ff3fb2f6bcdc6789b197ded
Jeffrey Lee <jlee42@gmail.com>
2018-07-23 13:53:53 -0500

    Adds support for hiding time parts

--- src/countdown-timer.component.ts
+++ src/countdown-timer.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input, OnInit, OnDestroy, ElementRef, Output,  EventEmitter  } from '@angular/core';
-
+import {
+  Component, Input, OnInit, OnDestroy, ElementRef, Output,  EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'countdown-timer',
   template: `{{ displayTime }}`
 })
-export class CountdownTimer implements OnInit, OnDestroy{
- 
+export class CountdownTimer implements OnInit, OnDestroy {
   @Input() start;
   @Input() end;
+  @Input() hours = true;
+  @Input() minutes = true;
+  @Input() seconds = true;
   @Output() zeroTrigger;
   @Input() timeOnly;
   timer: any;
@@ -17,18 +20,16 @@ export class CountdownTimer implements OnInit, OnDestroy{
     private el: ElementRef
   ) {
     this.zeroTrigger = new EventEmitter(true);
-
   }
 
   ngOnInit(): void {
     this.timer = setInterval(() => {
-      
-        if (this.start) {
-            this.displayTime = this.getTimeDiff(this.start, true);
-        } else {
-            this.displayTime = this.getTimeDiff(this.end);
-        }
-      }, 1000);
+      if (this.start) {
+          this.displayTime = this.getTimeDiff(this.start, true);
+      } else {
+          this.displayTime = this.getTimeDiff(this.end);
+      }
+    }, 1000);
   }
 
   ngOnDestroy() {
@@ -36,53 +37,54 @@ export class CountdownTimer implements OnInit, OnDestroy{
   }
 
   private getTimeDiff( datetime, useAsTimer = false ) {
-      datetime = new Date( datetime ).getTime();
-      var now = new Date().getTime();
-  
-      if( isNaN(datetime) )
-      {
-          return "";
-      }
+    datetime = new Date( datetime ).getTime();
+    var now = new Date().getTime();
 
-      var milisec_diff = datetime - now;
-      if (useAsTimer) {
-          milisec_diff = now - datetime;
-      } 
+    if (isNaN(datetime)) { return ''; }
 
-      // Zero Time Trigger
-      if (milisec_diff <= 0) {
-        this.zeroTrigger.emit("reached zero");
-        return "00:00:00:00";
-      }
+    var milisec_diff = datetime - now;
+    if (useAsTimer) { milisec_diff = now - datetime; }
 
-      var days = Math.floor(milisec_diff / 1000 / 60 / (60 * 24));
-      var date_diff = new Date( milisec_diff );
-      var day_string = (days) ? this.twoDigit(days) + ":" : "";
-      var day_hours = days * 24;
+    // Zero Time Trigger
+    if (milisec_diff <= 0) {
+      this.zeroTrigger.emit('reached zero');
+      return '00:00';
+    }
 
+    var days = Math.floor(milisec_diff / 1000 / 60 / (60 * 24));
+    var date_diff = new Date( milisec_diff );
+    var day_string = (days) ? this.twoDigit(days) + ':' : '';
+    var day_hours = days * 24;
+    var timeString = '';
 
-      if (this.timeOnly) {
-        let hours = date_diff.getUTCHours() + day_hours;
-        return  this.twoDigit(hours) +
-        ":" + this.twoDigit(date_diff.getMinutes()) + ":" 
-        + this.twoDigit(date_diff.getSeconds());
-      } else {
-        // Date() takes a UTC timestamp – getHours() gets hours in local time not in UTC. therefore we have to use getUTCHours()
-        return day_string + this.twoDigit(date_diff.getUTCHours()) +
-           ":" + this.twoDigit(date_diff.getMinutes()) + ":" 
-           + this.twoDigit(date_diff.getSeconds());
+    if (!this.timeOnly) {
+      timeString = day_string + ' ';
+    }
 
-      }
+    if (this.hours) {
+      var hours = date_diff.getUTCHours() + day_hours;
+      timeString = timeString + this.twoDigit(hours);
+    }
+
+    if (this.minutes) {
+      var minutes = date_diff.getMinutes();
+      timeString = timeString + ':' + this.twoDigit(minutes);
+    }
+
+    if (this.seconds) {
+      var seconds = date_diff.getSeconds();
+      timeString = timeString + ':' + this.twoDigit(seconds);
+    }
+
+    return timeString;
   }
 
-
   private twoDigit(number: number) {
-      return number > 9 ? "" + number: "0" + number;
+    return number > 9 ? '' + number: '0' + number;
   }
 
   private stopTimer() {
     clearInterval(this.timer);
     this.timer = undefined;
   }
-  
 }
